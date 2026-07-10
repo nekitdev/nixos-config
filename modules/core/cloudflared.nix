2e77d5af63691d288c277dffb35b32bb101e0e90
Nikita Tikhonov <nekit@nekit.dev>
2026-07-10 21:31:55 +0300

    Add `ingress` and `default` to `cloudflared`.

--- modules/core/cloudflared.nix
+++ modules/core/cloudflared.nix
@@ -5,6 +5,17 @@
     tunnels = {
       "49c463b3-1020-43d1-bcdf-9a62f1b5fe98" = {
         credentialsFile = "${config.sops.secrets.cloudflared.path}";
+
+        ingress = {
+          "nekit.dev" = "http://127.0.0.1:6942";
+          "ssh.nekit.dev" = "ssh://127.0.0.1:22";
+          "battery.nekit.dev" = "http://127.0.0.1:4269";
+          "lyrichar.app" = "http://127.0.0.1:4213";
+          "open.lyrichar.app" = "http://127.0.0.1:1369";
+          "api.lyrichar.app" = "http://127.0.0.1:1342";
+        };
+
+        default = "http_status:503";
       };
     };
   };
