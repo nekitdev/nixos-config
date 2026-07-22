42673c198bcbf0dc601be8b911889e3a02731521
Nikita Tikhonov <nekit@nekit.dev>
2026-07-22 23:49:05 +0300

    Troubleshoot `pi`.

--- hosts/pi/hardware.nix
+++ hosts/pi/hardware.nix
@@ -4,4 +4,11 @@ _: {
     initrd.systemd.enable = true;
     zfs.forceImportRoot = false;
   };
+
+  # ignore partitions with "required partition" attribute
+  services.udev.extraRules = ''
+    ENV{ID_PART_ENTRY_SCHEME}=="gpt", \
+      ENV{ID_PART_ENTRY_FLAGS}=="0x1", \
+      ENV{UDISKS_IGNORE}="1"
+  '';
 }
