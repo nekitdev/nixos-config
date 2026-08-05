7c305a3bbbae2fd27669851bf407fa841764f590
Nikita Tikhonov <nekit@nekit.dev>
2026-08-05 20:08:41 +0300

    Update `rtw89` hash.

--- pkgs/rtw89.nix
+++ pkgs/rtw89.nix
@@ -22,7 +22,7 @@ stdenv.mkDerivation {
     owner = "morrownr";
     repo = "rtw89";
     rev = "main";
-    sha256 = "sha256-tBW2TJjqwJRyxqfLAlqHtV9h6oLIBbU+10o57sJK4Sc=";
+    sha256 = "sha256-jr/yNxodrsdVHm5J0UlEOu43x1Ol9cNKw2O7jZlgpPQ=";
   };
 
   nativeBuildInputs = kernel.moduleBuildDependencies;
