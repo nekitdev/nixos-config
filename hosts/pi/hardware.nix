52b2d3b823d9b36161d803cb07c67573e5ea9133
Nikita Tikhonov <nekit@nekit.dev>
2026-08-07 10:56:32 +0300

    Delay `modprobe vc4`.

--- hosts/pi/hardware.nix
+++ hosts/pi/hardware.nix
@@ -1,8 +1,19 @@
 _: {
   boot = {
+    blacklistedKernelModules = [ "vc4" ]; # `modprobe` later
     loader.raspberry-pi.bootloader = "kernelboot";
     initrd.systemd.enable = true;
     zfs.forceImportRoot = false;
+  };
+
+  systemd.services.modprobe-vc4 = {
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+    before = [ "multi-user.target" ];
+    wantedBy = [ "multi-user.target" ];
+    script = "/run/current-system/sw/bin/modprobe vc4";
   };
 
   # ignore partitions with "required partition" attribute
